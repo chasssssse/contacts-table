9c740cd0d49c08eac873ca433e5498bd1e66e265
Chase Wang <Chase_Wang@epam.com>
2020-08-05 15:04:16 +0800

    feat: init server with express

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [res, setRes] = React.useState({ data: null });
+  const getRes = async () => (await fetch('/api/test')).json()
+
+  React.useEffect(() => {
+    getRes().then(res => setRes(res));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{res.data}</p>
       </header>
     </div>
   );
